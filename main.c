7f08bc0bc99e2b8b2b8866b45b727a48a01a018f
rumii <rumii_x@protonmail.com>
2022-10-09 01:02:36 +0530

    Added minimum capacity to process meta commands and sql commands

--- main.c
+++ main.c
@@ -3,16 +3,38 @@
 #include <string.h>
 #include <stdbool.h>
 
+/*------                          Enums and structs  ----------*/
 typedef struct {
     char *buffer;
     size_t buffer_length;
     ssize_t input_length;
 } InputBuffer;
 
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+}MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+}PrepareResult;
+
+/*-------------         Function declarations       --------------*/
+
 void display_prompt(void);
 InputBuffer* new_input_buffer(void);
 void close_input_buffer(InputBuffer* buffer);
 void read_line(InputBuffer* buffer);
+MetaCommandResult do_meta_command(InputBuffer *);
+PrepareResult prepare_statement(char *, Statement *);
+void execute_statement(Statement *statement);
 
 int main()
 {
@@ -21,14 +43,31 @@ int main()
         display_prompt();
         InputBuffer* buffer = new_input_buffer();
         read_line(buffer);
-        if (strcmp(buffer->buffer, ".exit") == 0)
+        if (buffer->buffer[0] == '.')                               //if meta command execute it
         {
-            close_input_buffer(buffer);
-            exit(EXIT_SUCCESS);
+            switch(do_meta_command(buffer)) {
+                case(META_COMMAND_SUCCESS):
+                    break;
+                case(META_COMMAND_UNRECOGNIZED_COMMAND):
+                {
+                    printf("%s : Unrecognized meta command\n",buffer->buffer);
+                    break;
+                }
+            }
         }
-        else
-        {
-            printf("%s :Unrecognized command\n", buffer->buffer);
+        else {
+
+            Statement statement;
+            switch (prepare_statement(buffer->buffer, &statement)) {
+                case(PREPARE_SUCCESS):
+                    execute_statement(&statement);
+                    break;
+                case(PREPARE_UNRECOGNIZED_STATEMENT):
+                {
+                    printf("%s : Unrecognized keyword at the start\n",buffer->buffer);
+                    break;
+                }
+            }
         }
     }
 }
@@ -65,4 +104,56 @@ void close_input_buffer(InputBuffer* buffer)
 {
     free(buffer->buffer);
     free(buffer);
+}
+
+
+/* branch over different meta commands and
+   execute if it is a valid meta command
+*/
+MetaCommandResult do_meta_command(InputBuffer *buffer)
+{
+    if (strcmp(buffer->buffer,".exit") == 0)
+    {
+        close_input_buffer(buffer);
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        printf("%s: Unrecognized meta command\n", buffer->buffer);
+    }
+}
+
+/* branch over valid keywords and
+   pass statements type
+*/
+PrepareResult prepare_statement(char *buffer, Statement *statement)
+{
+    /* strncmp is used instead of strcmp because
+       keyword will be followed by arguments
+    */
+    if (strncmp(buffer, "insert", 6) == 0)
+    {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    else if (strncmp(buffer, "select", 6) == 0)
+    {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    else
+        return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement)
+{
+    if (statement->type = STATEMENT_INSERT)
+    {
+        printf("Insertion\n");
+    }
+    else if (statement->type = STATEMENT_SELECT)
+    {
+        printf("Selection\n");
+    }
+    return;
 }
